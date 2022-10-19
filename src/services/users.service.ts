26c8bf4fdf08205a392fd5394ff0f81853d0dbb6
Vlad Tancev <vlad.tancev@gmail.com>
2022-10-19 22:01:06 +0300

    Sequalize: off repository mode

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,15 +1,9 @@
-import {sequelize} from "../common/sequalize";
 import {User} from "../models/users.model";
 
 export class UsersService {
-    private usersRepository = sequelize.getRepository(User);
-
-    constructor() {
-        this.usersRepository.sync()
-    }
 
     async getAll() {
-        const result = await this.usersRepository.findAll({});
+        const result = await User.findAll({});
         return result;
     }
 
